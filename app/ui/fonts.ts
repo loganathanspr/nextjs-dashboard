d54b399a11fed3fa20c9da28422976766b777ae2
loganathanspr <loganathanspr@gmail.com>
2024-07-14 16:08:39 +0200

    Dashboard and fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Open_Sans, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const opensans = Open_Sans({
+  subsets: ["latin"],
+});
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
